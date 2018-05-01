72a1540276bc25e0683a3e1f3d121efc2fc26da8
Bill Rawlinson <bill.rawlinson@sbcs.com>
2018-05-01 13:04:54 -0400

    update lodash

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ System.config({
     'babel-runtime': 'npm:babel-runtime@5.8.20',
     'bluebird': 'npm:bluebird@2.9.34',
     'core-js': 'npm:core-js@1.1.0',
-    'lodash': 'npm:lodash@3.10.1',
+    'lodash': 'npm:lodash@4.17.10',
     'github:jspm/nodelibs-process@0.1.1': {
       'process': 'npm:process@0.10.1'
     },
@@ -31,9 +31,6 @@ System.config({
       'fs': 'github:jspm/nodelibs-fs@0.1.2',
       'process': 'github:jspm/nodelibs-process@0.1.1',
       'systemjs-json': 'github:systemjs/plugin-json@0.1.0'
-    },
-    'npm:lodash@3.10.1': {
-      'process': 'github:jspm/nodelibs-process@0.1.1'
     }
   }
 });
